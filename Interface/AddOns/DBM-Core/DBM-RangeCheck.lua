83927e895990f43b3fb6bedbd0ce946d7d02949c
Funsponge <jimmeh@funsponge.net>
2013-09-10 17:06:56 +0100

    omnicc for 5.4 + dbm range check tweaks

--- Interface/AddOns/DBM-Core/DBM-RangeCheck.lua
+++ Interface/AddOns/DBM-Core/DBM-RangeCheck.lua
@@ -433,7 +433,7 @@ function createRadarFrame()
 	local bg = radarFrame:CreateTexture(nil, "BACKGROUND")
 	bg:SetAllPoints(radarFrame)
 	bg:SetBlendMode("BLEND")
-	bg:SetTexture(0, 0, 0, 0.3)
+	bg:SetTexture(0, 0, 0, 0)
 	radarFrame.background = bg
 
 	local circle = radarFrame:CreateTexture(nil, "ARTWORK")
